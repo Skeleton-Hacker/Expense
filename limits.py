a619e17c921f6bccb55f2563be3a7a50cdf44890
Skeleton-Hacker <156354631+Skeleton-Hacker@users.noreply.github.com>
2024-07-11 19:05:02 +0530

    Rectify_2

--- limits.py
+++ limits.py
@@ -20,24 +20,30 @@ def limits():
 def check_exceed(month, year):
     choice = -1
     if input("Select (y) to check for week: ") == 'y':
-        choice = 0
-    else:
         choice = 1
+    else:
+        choice = 0
 
     file = os.path.join("expenses/expenses_" + month + year + ".txt")
     data = reader(file)
     expense = 0
     for line in data:
-        expense += int(line.split(",")[0])
+        try:
+            expense += int(line.split(",")[0])
+        except:
+            pass
     print(f"Expenditure in month till now: {expense}")
-    
+
     file = os.path.join("expenses/expenses_limits.txt")
     data = reader(file)
+    
     if choice:
-        limit = data[1].split(" ")[2]
-        if expense >= limit:
-            print("Monthly expense limit exceeded.")
-    else:
-        limit = data[0].split(" ")[2]
+        limit = int(data[0].split(" ")[2])
+        print(f"Weekly Budget left: {limit + expense}")
         if expense >= limit:
             print("Weekly expense exceeded.")
+
+    limit = int(data[1].split(" ")[2])
+    print(f"Monthly Budget left: {limit + expense}")
+    if expense >= limit:
+        print("Monthly expense limit exceeded.")
--- main.py
+++ main.py
@@ -5,28 +5,33 @@ from helper import *
 def options():
     print("Select option:")
     print("\t 1. Add entry")
-    print("\t 2. Current Month Expense")
-    print("\t 3. Monthly expense overview")
-    print("\t 4. Filter")
+    # print("\t 2. Current Month Expense")
+    # print("\t 3. Monthly expense overview")
+    # print("\t 4. Filter")
     print("\t 5. Set Expense limits")
+    print("To exit, press 'Ctrl + C'")
     return int(input().strip())
 
 def main():
-    choice = options()
-    clear_screen()
-    month = input("Month: ")
-    year = input("Year: ")
-    if choice == 1:
-        add_entry(month, year)
-    elif choice == 2:
-        print("Select option:")
-        print("\t a. Month")
-        print("\t b. Variance over Months")
-        print("\t c. ")
-    elif choice == 5:     
-        limits()
-    
-    check_exceed(month, year)
+    while True:
+        try:
+            clear_screen()
+            month = input("Month: ")
+            year = input("Year: ")
+            clear_screen()
+            choice = options()
+            clear_screen()
+            if choice == 1:
+                add_entry(month, year)
+            elif choice == 5:     
+                limits()
+        except KeyboardInterrupt:
+            clear_screen()
+            print("All changes have been saved successfully")
+            check_exceed(month, year)
+            break
+        except:
+            print("Invalid command, try again")
 
 if __name__ == "__main__":
     main()
