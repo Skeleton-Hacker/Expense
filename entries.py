cd8fd6c676fa8224b0b4b64b8dd2acb45dbe9d82
Skeleton-Hacker <156354631+Skeleton-Hacker@users.noreply.github.com>
2024-07-11 15:09:50 +0530

    subdivide_1

---
+++ entries.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+expense_file = "expense.txt"
+
+def write_entry(entry):
+    with open(expense_file, "a") as file:
+        file.write(entry)
+
+def add_entry():
+    while(1):
+        while(1):    
+            type = input("Select Credit(+) or Debit(-): ").strip()
+            if type == "+" or "-":
+                break
+            else:
+                print("Invalid response,", end = " ")
+
+        while(1):
+            try:
+                amount = int(input("Enter amount: "))
+                break
+            except ValueError:
+                print("Invalid amount")
+
+        while(1):    
+            date = input("Enter date(DD-MM-YY): ").strip()
+            try:
+                datetime.strptime(date, "%d-%m-%y")
+                break
+            except ValueError:
+                print("Incorrect date format, use (DD-MM-YY)")
+
+        tag = input("Enter type of expense: ").strip()
+        entry = type + amount + "," + date + "," + tag + "\n"
+        print("Confirm entry(y) or skip(n):", entry, end = "")
+        if input().strip() == 'y':
+            write_entry(entry)
+            break
+
+add_entry()
+
--- main.py
+++ main.py
@@ -1,33 +1,13 @@
-expense_file = "expense.txt"
+from entries import *
 
 def options():
     print("Select option:")
     print("\t 1. Add entry")
     print("\t 2. Delete entry")
     print("\t 3. Filter")
-    return input().strip()
+    return int(input().strip())
 
-def write_entry(entry):
-    with open(expense_file, "a") as file:
-        file.write(entry)
-
-def add_entry():
-    while(1):
-        while(1):    
-            type = input("Select Credit(+) or Debit(-): ").strip()
-            if type == "+" or "-":
-                break
-        try:
-            amount = int(input("Enter amount: "))
-        except:
-            pass
-        date = input("Enter date(DD-MM-YY): ").strip()
-        tag = input("Enter type of expense: ").strip()
-        entry = type + amount + "," + date + "," + tag + "\n"
-        print("Confirm entry(y) or skip(n):", entry )
-        if input().strip() == 'y':
-            write_entry(entry)
-            break
-
-add_entry()
-
+def main():
+    choice = options()
+    if choice == 1:
+        add_entry()
